601815a3f776f0228cc03c61b248c3480b2aaaff
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-01-30 20:01:33 -0800

    Adjust index, Product, and ProductTag models

--- models/index.js
+++ models/index.js
@@ -5,25 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.hasOne(Category, {
+Product.hasOne(Category, {
   foreignKey: 'Product_id',
   onDelete: 'CASCADE',
 });
 
 
 // Categories have many Products
-Cagegory.belongsTo(Products, {
+Category.belongsTo(Product, {
   foreignKey: 'Product_id',
 });
 
 
 // Products belongToMany Tags (through ProductTag)
-Products.belongsTo(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
-Catetgories.hasMany(Products, {
+Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
