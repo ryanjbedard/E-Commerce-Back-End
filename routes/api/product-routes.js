272148f2d6b5020c8d66de524dd27c9119e5e386
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-01-31 17:20:04 -0800

    add delete functionality to product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -83,7 +83,7 @@ router.put('/:id', (req, res) => {
         
         ProductTag.findAll({
           where: { product_id: req.params.id }
-        }).then((ProductTags) => {
+        }).then((ProductTag) => {
           // create filtered list of new tag_ids
           const productTagIds = ProductTag.map(({ tag_id }) => tag_id);
           const newProductTags = req.body.tagIds
@@ -115,8 +115,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
